0bc0d3fda2cf5df4d3a5918849d939b0cb750dc2
Daniele Madama <d.madama@gmail.com>
2015-06-24 17:22:15 +0200

    WIP

---
+++ crepuscolo-validation/src/main/java/net/etalia/crepuscolo/validation/ValidationMessage.java
@@ -0,0 +1,10 @@
+package net.etalia.crepuscolo.validation;
+
+public class ValidationMessage {
+
+	public static final String REQUIRED = "_REQUIRED";
+	public static final String LENGTH = "_LENGTH";
+	public static final String MALFORMED = "_MALFORMED";
+	public static final String RANGE_NOT_VALID = "_RANGE_NOT_VALID";
+
+}
---
+++ crepuscolo-validation/src/main/java/net/etalia/crepuscolo/validation/ValidationRegexp.java
@@ -0,0 +1,7 @@
+package net.etalia.crepuscolo.validation;
+
+public class ValidationRegexp {
+
+	public static final String URL = "^$|^(?:http:|https:|)//[-'\\p{L}\\u2019\\p{Nd}+&@#/\\*\\$\\(\\)%?=~_|!:,\\.;]*";
+
+}
