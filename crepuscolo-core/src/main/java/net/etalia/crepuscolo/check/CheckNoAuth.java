5bca48f6dc57175cfff9f00b72d71c47521a2912
Simone Gianni <simoneg@apache.org>
2016-03-21 18:48:43 +0100

    Missing annotations for checkers

---
+++ crepuscolo-core/src/main/java/net/etalia/crepuscolo/check/CheckNoAuth.java
@@ -0,0 +1,10 @@
+package net.etalia.crepuscolo.check;
+
+/**
+ * Check annotation that forces not to consider current auth status.
+ * 
+ * @author Simone Gianni <simoneg@apache.org>
+ */
+public @interface CheckNoAuth {
+
+}
---
+++ crepuscolo-core/src/main/java/net/etalia/crepuscolo/check/CheckNoChecks.java
@@ -0,0 +1,10 @@
+package net.etalia.crepuscolo.check;
+
+/**
+ * Check annotation that forces to bypass all existing checks.
+ * 
+ * @author Simone Gianni <simoneg@apache.org>
+ */
+public @interface CheckNoChecks {
+
+}
