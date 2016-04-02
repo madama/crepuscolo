e848c6dcceef967c77b081d44b41a3766928fc8d
Daniele Madama <d.madama@gmail.com>
2016-04-02 14:04:40 +0200

    Fix test

--- crepuscolo-aws/src/test/java/net/etalia/crepuscolo/queue/DummyBean.java
+++ crepuscolo-aws/src/test/java/net/etalia/crepuscolo/queue/DummyBean.java
@@ -1,50 +1,34 @@
 package net.etalia.crepuscolo.queue;
 
-import javax.persistence.Entity;
-import javax.validation.constraints.NotNull;
-import javax.validation.groups.Default;
-
-import org.hibernate.validator.constraints.Email;
-import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.Range;
-
-import net.etalia.crepuscolo.auth.MockCheck;
 import net.etalia.crepuscolo.domain.BaseEntity;
 import net.etalia.jalia.annotations.JsonDefaultFields;
 
 @JsonDefaultFields("name,email,age")
-@Entity
 public class DummyBean extends BaseEntity {
 
 	private String name;
 	private String email;
 	private int age;
-	
-	
-	@NotNull(message="Name is required", groups={Default.class, DummyBean.class})
-	@Length(min=2,max=50)
+
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	@Email
+
 	public String getEmail() {
 		return email;
 	}
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
-	@Range(min=18,max=110,message="Too young or too old")
-	@MockCheck(testValue="getterCheck")
+
 	public int getAge() {
 		return age;
 	}
 	public void setAge(int age) {
 		this.age = age;
 	}
-	
+
 }
