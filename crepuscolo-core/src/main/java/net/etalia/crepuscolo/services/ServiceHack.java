a3ef3dd75331db6b41a9c0ac5dfbe70b5b8b4a09
Daniele Madama <d.madama@gmail.com>
2016-04-07 19:33:48 +0200

    ServiceHack improvements

--- crepuscolo-core/src/main/java/net/etalia/crepuscolo/services/ServiceHack.java
+++ crepuscolo-core/src/main/java/net/etalia/crepuscolo/services/ServiceHack.java
@@ -2,9 +2,13 @@ package net.etalia.crepuscolo.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.ApplicationContext;
 
 @Configurable
 public class ServiceHack {
+
+	@Autowired
+	private ApplicationContext appctx;
 
 	private static ServiceHack instance = null;
 
@@ -12,11 +16,6 @@ public class ServiceHack {
 		if (instance == null) {
 			instance = new ServiceHack();
 		}
-		return instance;
-	}
-
-	public static ServiceHack createNewInstance() {
-		instance = new ServiceHack();
 		return instance;
 	}
 
@@ -29,16 +28,29 @@ public class ServiceHack {
 	@Autowired(required=false)
 	private AuthService authService;
 
+	@Deprecated
 	public StorageService getStorageService() {
 		return storageService;
 	}
 
+	@Deprecated
 	public CreationService getCreationService() {
 		return creationService;
 	}
 
+	@Deprecated
 	public AuthService getAuthService() {
 		return authService;
+	}
+
+	public <T> T getBean(Class<T> clazz) {
+		if (appctx == null) return null;
+		return appctx.getBean(clazz);
+	}
+
+	public Object getBean(String name) {
+		if (appctx == null) return null;
+		return appctx.getBean(name);
 	}
 
 }
