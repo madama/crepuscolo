0a08418c3045c3548491b2bfce1f114f051838d6
Daniele Madama <d.madama@gmail.com>
2016-05-19 18:22:12 +0200

    Add RequestLoggerFilter

---
+++ crepuscolo-web/src/main/java/net/etalia/crepuscolo/mvc/RequestLoggerFilter.java
@@ -0,0 +1,46 @@
+package net.etalia.crepuscolo.mvc;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class RequestLoggerFilter implements Filter {
+
+	protected Log log = LogFactory.getLog(RequestLoggerFilter.class);
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		long start = System.currentTimeMillis();
+		chain.doFilter(request, response);
+		long end = System.currentTimeMillis();
+		long time = end - start;
+		String url = null;
+		String queryString = null;
+		String method = null;
+		if (request instanceof HttpServletRequest) {
+			url = ((HttpServletRequest)request).getContextPath();
+			url += ((HttpServletRequest)request).getServletPath();
+			queryString = ((HttpServletRequest)request).getQueryString();
+			method = ((HttpServletRequest)request).getMethod();
+		}
+		log.info("TIME: " + time + " - METHOD: " + method + " - URL: " + url + " - PARAMS: " + queryString);
+	}
+
+	@Override
+	public void destroy() {
+	}
+
+}
